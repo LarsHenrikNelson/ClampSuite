e8f193a4a06109d2f260f0a4db6aae032d858715
Lars Henrik Nelson <larshnelson@protonmail.com>
2022-10-21 15:07:29 -0400

    "Updated sphinx docs."

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ClampSuite'
-copyright = '2022, Lars Henrik Nelson'
-author = 'Lars Henrik Nelson'
-release = '0.0.2'
+project = "ClampSuite"
+copyright = "2022, Lars Henrik Nelson"
+author = "Lars Henrik Nelson"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_css_files = [
+    "custom.css",
+]
