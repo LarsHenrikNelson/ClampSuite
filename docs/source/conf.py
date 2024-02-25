9c01cc41dbe39dd8e80efc3603140ae892435f38
Lars Henrik Nelson <larshnelson@protonmail.com>
2024-02-25 17:45:15 -0500

    updated readthedocs requirements.txt and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,14 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+
 project = "ClampSuite"
 copyright = "2022, Lars Henrik Nelson"
 author = "Lars Henrik Nelson"
 release = "0.0.4"
+
+html_logo = os.path.join("images", "logo.png")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
