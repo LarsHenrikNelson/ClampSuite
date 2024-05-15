4c465e57d8ef4199638b9414522ce379de2dde0f
Lars Henrik Nelson <larshnelson@protonmail.com>
2024-05-15 16:41:04 -0400

    Updated tests

--- tests/exp_manager_test.py
+++ tests/exp_manager_test.py
@@ -1,36 +1,40 @@
 from clampsuite.manager import ExpManager
 
+import pytest
+
 
 def test_exp_manager_data():
     assert len(ExpManager.windows) > 0
     assert len(ExpManager.filters) > 0
 
 
-def test_current_clamp_data(current_clamp_data_scanimage):
+@pytest.mark.parametrize(
+    "data, name",
+    [
+        ("current_clamp_data_scanimage", "current_clamp"),
+        ("msn_mepsc_data_scanimage", "mini"),
+        ("oepsc_data_scanimage", "oepsc"),
+        ("olfp_data_scanimage", "lfp"),
+    ],
+)
+def test_create_exp(request, data, name):
+    result = request.getfixturevalue(data)
     exp_manager = ExpManager()
-    exp_manager.create_exp("current_clamp", current_clamp_data_scanimage)
-    assert "current_clamp" in exp_manager.exp_dict.keys()
-    assert len(exp_manager.exp_dict["current_clamp"].items()) == len(
-        current_clamp_data_scanimage
-    )
+    exp_manager.create_exp(name, result)
+    assert name in exp_manager.exp_dict.keys()
 
 
-def test_mini_analysis_data(msn_mepsc_data_scanimage):
+@pytest.mark.parametrize(
+    "data, name",
+    [
+        ("current_clamp_data_scanimage", "current_clamp"),
+        ("msn_mepsc_data_scanimage", "mini"),
+        ("oepsc_data_scanimage", "oepsc"),
+        ("olfp_data_scanimage", "lfp"),
+    ],
+)
+def test_mini_analysis_data(request, data, name):
+    result = request.getfixturevalue(data)
     exp_manager = ExpManager()
-    exp_manager.create_exp("mini", msn_mepsc_data_scanimage)
-    assert "mini" in exp_manager.exp_dict.keys()
-    assert len(exp_manager.exp_dict["mini"].items()) == len(msn_mepsc_data_scanimage)
-
-
-def test_oepsc_analysis_data(oepsc_data_scanimage):
-    exp_manager = ExpManager()
-    exp_manager.create_exp("oepsc", oepsc_data_scanimage)
-    assert "oepsc" in exp_manager.exp_dict.keys()
-    assert len(exp_manager.exp_dict["oepsc"].items()) == len(oepsc_data_scanimage)
-
-
-def test_lfp_analysis_data(olfp_data_scanimage):
-    exp_manager = ExpManager()
-    exp_manager.create_exp("lfp", olfp_data_scanimage)
-    assert "lfp" in exp_manager.exp_dict.keys()
-    assert len(exp_manager.exp_dict["lfp"].items()) == len(olfp_data_scanimage)
+    exp_manager.create_exp(name, result)
+    assert len(exp_manager.exp_dict[name].items()) == len(result)
