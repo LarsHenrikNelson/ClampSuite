11bd32ee765742a8476df5e6605e54d4afd49f9c
Lars Henrik Nelson <larshnelson@protonmail.com>
2025-07-13 13:09:06 -0400

    Updated log_fit to improve fits.

--- clampsuite/functions/curve_fit/log_fit.py
+++ clampsuite/functions/curve_fit/log_fit.py
@@ -7,6 +7,7 @@ from scipy.optimize import curve_fit
 class LogCurveFit(NamedTuple):
     vscale: float
     offset: float
+    xshift: float
 
 
 def log_func(x, vscale, offset=0, xshift=0):
@@ -15,18 +16,23 @@ def log_func(x, vscale, offset=0, xshift=0):
 
 def fit_log(x, y):
     try:
-        p0 = [np.max(y) - np.min(y) / np.log(x[-1] - x[0]), np.min(y), 0]
-        ub = [np.inf, np.inf, np.min(x) - 1e-6]
+        xmin= np.min(x)
+        xmax = np.max(x)
+        divisor = max((xmax - xmin), 1)
+        p0 = [np.max(y) - np.min(y) / max(np.log(divisor), 1), np.min(y), 0]
+        ub = [np.inf, np.inf, xmin - 1e-6]
         lb = [-np.inf, -np.inf, -np.inf]
         p, _ = curve_fit(log_func, x, y, p0=p0, bounds=(lb, ub))
 
         output = LogCurveFit(
             vscale=p[0],
             offset=p[1],
+            xshift=p[2],
         )
     except Exception:
         output = LogCurveFit(
             vscale=np.nan,
             offset=np.nan,
+            xshift=np.nan,
         )
     return output
