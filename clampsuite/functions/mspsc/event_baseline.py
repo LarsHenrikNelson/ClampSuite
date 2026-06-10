f8a975b9635fe812720a996102096d76837e7c79
Lars Henrik Nelson <larshnelson@protonmail.com>
2026-06-10 09:24:39 -0400

    Changed find_baseline event_peak to peak_index.

--- clampsuite/functions/mspsc/event_baseline.py
+++ clampsuite/functions/mspsc/event_baseline.py
@@ -2,21 +2,21 @@ import numpy as np
 from scipy import signal
 
 
-def find_alt_baseline(event_array: np.ndarray, event_peak, fs) -> int:
+def find_alt_baseline(event_array: np.ndarray, peak_index: int, fs: float | int) -> int:
     s_r_c = fs / 1000
     baselined_array = event_array - np.mean(event_array[: int(1 * s_r_c)])
     masked_array = baselined_array.copy()
     mask = np.argwhere(baselined_array <= 0)
     masked_array[mask] = 0
-    peaks = signal.argrelmax(masked_array[0 : int(event_peak)], order=2)
+    peaks = signal.argrelmax(masked_array[0 : int(peak_index)], order=2)
     if len(peaks[0]) > 0:
         event_baseline = int(peaks[0][-1])
     else:
-        event_baseline = int(np.argmax(masked_array[0 : int(event_peak)]))
+        event_baseline = int(np.argmax(masked_array[0 : int(peak_index)]))
     return event_baseline
 
 
-def find_baseline(event_array: np.ndarray, event_peak: int, fs: float) -> int:
+def find_baseline(event_array: np.ndarray, peak_index: int, fs: float) -> int:
     """
     This functions finds the baseline of an event. The biggest issue with
     most methods that find the baseline is that they assume the baseline
@@ -25,21 +25,21 @@ def find_baseline(event_array: np.ndarray, event_peak: int, fs: float) -> int:
     finding method.
     """
     s_r_c = fs / 1000
-    baselined_array = event_array - np.max(event_array[:event_peak])
-    event_peak_y = event_array[event_peak]
+    baselined_array = event_array - np.max(event_array[:peak_index])
+    event_peak_y = event_array[peak_index]
     search_start = np.argwhere(
-        baselined_array[:event_peak] > 0.35 * event_peak_y
+        baselined_array[:peak_index] > 0.35 * event_peak_y
     ).flatten()
     if search_start.size > 0:
         slope = (event_array[search_start[-1]] - event_peak_y) / (
-            event_peak - search_start[-1]
+            peak_index - search_start[-1]
         )
         new_slope = slope + 1
         i = search_start[-1]
         while new_slope > slope and i > 0:
-            slope = (event_array[i] - event_peak_y) / (event_peak - i)
+            slope = (event_array[i] - event_peak_y) / (peak_index - i)
             i -= 1
-            new_slope = (event_array[i] - event_peak_y) / (event_peak - i)
+            new_slope = (event_array[i] - event_peak_y) / (peak_index - i)
         baseline_start = signal.argrelmax(
             baselined_array[int(i - 1 * s_r_c) : i + 2], order=2
         )[0]
@@ -52,5 +52,5 @@ def find_baseline(event_array: np.ndarray, event_peak: int, fs: float) -> int:
             if event_baseline < 0:
                 event_baseline = 0
     else:
-        event_baseline = find_alt_baseline(event_array, event_peak, fs=fs)
+        event_baseline = find_alt_baseline(event_array, peak_index, fs=fs)
     return event_baseline
