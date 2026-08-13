dc1610ff5fd61388afd62375d14beff9322c1eef
Lars Henrik Nelson <larshnelson@protonmail.com>
2026-08-13 15:43:20 -0400

    Fixed a typo for the label mapping.

--- clampsuite/functions/utilities.py
+++ clampsuite/functions/utilities.py
@@ -164,7 +164,7 @@ def map_keys(keys: Iterable) -> dict:
         "hz": "(Hz)",
         "ms": "(ms)",
         "pa/ms": "pA/ms",
-        "mv/ms": "pA/ms",
+        "mv/ms": "pV/ms",
         "index": "(ms)",
     }
     key_mapping = {}
